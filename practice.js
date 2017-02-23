3180ed5b3303b41570cae3b9a71b3f1fd3ff5f52
wuno <ndiaz@wuno.com>
2017-02-22 22:43:46 -0600

    Added practice functions

--- practice.js
+++ practice.js
@@ -100,8 +100,8 @@ var data = {
 function execData(data, callback) {
 
     function split(data, callback) {
-        for(key in data) {
-            if (key === 'a'){
+        for (key in data) {
+            if (key === 'a') {
                 var ns = data[key].split('').reverse();
                 callback(ns);
             }
@@ -112,3 +112,88 @@ function execData(data, callback) {
 
 // function call uncomment to run this function
 // execData(data);
+
+// Capatalie the first letter of each word in a string
+
+var sentance = "hey you";
+
+function LetterCapitalize(str) {
+    var sep = str.split(' ');
+    for (var i = 0; i < sep.length; i++) {
+
+        sep[i] = sep[i].charAt(0).toUpperCase() + sep[i].substring(1);
+    }
+    str = sep.join(" ");
+
+    return str;
+}
+
+// uncomment to output the LetterCapitalize() function
+// console.log(LetterCapitalize(sentance));
+
+// reverse a string
+
+var myStr "This is a string";
+
+function FirstReverse(str) {
+    str = str.split('');
+    str.reverse().join();
+
+    return str;
+}
+
+// uncomment to output FirstReverse() 
+// FirstReverse(myStr;
+
+// Change each letter in a string to the next leeter int he alpabet
+// If any of the letters are vowels capatalize them
+var lcStr = "This is another string";
+
+function LetterChanges(str) {
+    str = str.split('');
+    for (var i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case ' ':
+                break;
+            case 'z':
+                str[i] = 'A';
+            case 'Z':
+                str[i] = 'A';
+                break;
+            default:
+                str[i] = String.fromCharCode(1 + str[i].charCodeAt(0));
+        }
+        switch (str[i]) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                str[i] = str[i].toUpperCase();
+        }
+    }
+    str = str.join('');
+    // code goes here  
+    return str;
+
+}
+
+// Uncomment below to output LetterChange()
+// console.log(LetterChange(lcStr));
+
+// Take an int and then loop through it adding the times you loop together till you 
+// reach the value of the number
+// 4 would be 1 + 2 + 3 + 4 = 10
+var myInt = 4;
+
+function SimpleAdding(num) {
+    var x = 0;
+    for (var i = 0; i < num; i++) {
+        x = i + x;
+        x++;
+    }
+    return x;
+}
+
+// uncomment to run SimpleAdding
+// console.log(SimpleAdding(myInt));
